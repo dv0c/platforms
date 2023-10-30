46afb5c496647ef3d91bdea4ae8f7d6c17580937
dv0c <tasosmeidanis12@gmail.com>
2023-10-30 10:27:38 +0200

    deleted:    .eslintrc.cjs 	modified:   package-lock.json 	modified:   package.json 	modified:   src/app/api/auth/[...nextauth]/route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 
 import { authOptions } from "@/server/auth";
 
+// @typescript-eslint/no-unsafe-assignment
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
