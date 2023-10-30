3f1fec15e9bd12d8f0de3f3823201605d392f808
dv0c <tasosmeidanis12@gmail.com>
2023-10-30 10:56:26 +0200

    modified:   src/server/auth.ts

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -42,7 +42,9 @@ export const authOptions: NextAuthOptions = {
         sameSite: "lax",
         path: "/",
         // When working on localhost, the cookie domain must be omitted entirely (https://stackoverflow.com/a/1188145)
-        domain: VERCEL_DEPLOYMENT ? `.localhost` : undefined,
+        domain: VERCEL_DEPLOYMENT
+          ? `.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`
+          : undefined,
         secure: VERCEL_DEPLOYMENT,
       },
     },
