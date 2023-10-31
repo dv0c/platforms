aa64ec5685dee48cf75d14da8703dc90171a324b
dv0c <tasosmeidanis12@gmail.com>
2023-10-31 14:33:16 +0200

    modified:   src/app/home/_components/Navbar.tsx

--- src/app/home/_components/Navbar.tsx
+++ src/app/home/_components/Navbar.tsx
@@ -3,6 +3,7 @@ import { Navigation } from "./Navigation";
 import Profile from "@/components/Profile";
 import { Button } from "@/components/ui/button";
 import { getSession } from "@/server/auth";
+import Link from "next/link";
 
 const Navbar = async () => {
   const session = await getSession();
@@ -16,7 +17,11 @@ const Navbar = async () => {
         </div>
         <Navigation />
         <div className="ml-auto flex items-center gap-3">
-          {session?.user && <Button variant={"ghost"}>Go to dashboard</Button>}
+          {session?.user && (
+            <Link href={process.env.NEXT_PUBLIC_APP_DOMAIN!}>
+              <Button variant={"ghost"}>Go to dashboard</Button>
+            </Link>
+          )}
           <Profile session={session} />
         </div>
       </nav>
