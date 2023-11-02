65c6ca73852fc3cb9ac4d76fbb2266c8651901c6
dv0c <tasosmeidanis12@gmail.com>
2023-11-02 14:16:36 +0200

    modified:   package-lock.json 	modified:   package.json 	modified:   src/app/layout.tsx 	renamed:    src/styles/globals.css -> src/styles/globals.scss

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import Providers from "@/components/Providers";
-import "@/styles/globals.css";
+import "@/styles/globals.scss";
 
-import { GeistSans, GeistMono } from "geist/font";
+import { GeistSans } from "geist/font";
 import { Toaster } from "sonner";
 
 export const metadata = {
